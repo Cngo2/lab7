3cbe2ef4872cd455801be96b8f6f69d255b2d03f
Cngo2 <97567147+Cngo2@users.noreply.github.com>
2022-04-04 23:10:38 -0500

    edited script with appropriate functions

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
-//add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+//add your javascrip between these two lines of code	
+	function alertFunction(){
+		var x1 = document.getElementById("entryinput").value;
+		var x2 = document.getElementById("textoutput");
+		alert("Casey Ngo: "+ x1);
+		x2.innerHTML = x1;
+	}
+	var x3 = document.getElementById("entrybutton");
+	x3.addEventListener("click", alertFunction);
+}
 window.addEventListener('load', init);
